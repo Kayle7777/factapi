bccafd2dbc2c1f0e517d25b4c6b9b253b46dee53
Kayle7777 <jessewebb2@gmail.com>
2019-06-23 17:23:57 -0500

    Ready to deploy, work out deployment pipeline

--- routes/facts.js
+++ routes/facts.js
@@ -1,27 +1,22 @@
 var express = require('express');
 var router = express.Router();
-const { facts, sequelize } = require('../models');
-
-/* GET home page. */
-router.get('/random', function(req, res, next) {
-    // Node on the fact[0] -- this only ever returns ONE fact. Keep in mind, a stored procedure is returning a TABLE (therefore array)
-    sequelize.query('CALL getRandomFact();', { model: facts }).then(fact => res.json(fact[0]));
-});
-
-router.get('/author/:author', function(req, res) {
-    sequelize
-        .query('CALL getFactsByAuthor(:author);', { model: facts, replacements: { author: req.params.author } })
-        .then(fact => res.json(fact));
-});
+const { Op, fact, sequelize, Sequelize } = require('../models');
 
 router.get('/query', function(req, res) {
-    const { search } = req.query;
-    sequelize
-        .query('CALL getRandomQueryFact(:text);', { model: facts, replacements: { text: search } })
-        .then(facts => res.json(facts[0])); // We send one because this is random from author.
+    const { query } = req;
+    const sqlQuery = {
+        order: Sequelize.literal('RAND()'),
+        where: {},
+    };
+
+    for (key in query) {
+        sqlQuery.where[key] = { [Op.like]: `%${query[key]}%` };
+    }
+    fact.findOne(sqlQuery).then(fact => res.json(fact));
 });
 
 ///TO-DO: Figure out some way to authenticate, can only come from discord or slack
+/// TO-DO: Add logic for rating
 router.post('/add', function(req, res) {
     //This needs to be x-www-form-urlencoded
     const { author, fact_text, source } = req.body;
@@ -37,13 +32,7 @@ router.post('/add', function(req, res) {
             }
         });
 });
-
-// Deletes the last fact in the db, but only if the author is the same as the one that sent it, and only if it is less than 1 day old.
-router.delete('/whoops', function(req, res) {
-    const { author } = req.body;
-    sequelize.query('CALL revertLastFact(:author)', { replacements: { author: author } }).then(ret => {
-        res.send(Boolean(ret[0]['@success']));
-    });
-});
+// Do I want to do this and continue on with the stored procedure thing? Or do I want to go full Sequelize API. Think on it.
+// arguments is an array, searchParams is an object.
 
 module.exports = router;
